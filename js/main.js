62508b8d45ea2b608ab2c91b6724173aac0de27a
Natalia Popova <ntl230189@gmail.com>
2024-10-25 16:29:09 +0500

    add ready module4-task1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
-/*
-const photoArray = [
-  {id: от 1 до 25,
-    url: 'photos/' + число от 1 до 25 +'.jpg',
-    description: 'Hello! It is my photo!',
-    likes: случайное число от 15 до 200,
-    comments: массив объектов от 0 до 30 [{
-       случайный набор,
-    }],}
-]*/
+const MESSAGE_IN_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const USERS_NAMES = [
+  'Vasiliy',
+  'Maria',
+  'Ivan',
+  'Anna',
+  'John',
+  'Eva',
+];
 
 const generateArray = (countElement) => [...Array(countElement).keys()].map((index)=>index + 1);
 
-const createNumberId = (countElement) => {
+const createNumberId = (countElement = '') => {
   let currentId;
   let indexId = 0;
+
+  if (countElement === '') {
+    return function () {
+      indexId += 1;
+      return indexId;
+    };
+  }
 
   return function (){
     currentId = generateArray(countElement)[indexId];
@@ -22,7 +36,7 @@ const createNumberId = (countElement) => {
   };
 };
 
-const generateRandomInteger = (min, max) => {
+const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
@@ -30,42 +44,30 @@ const generateRandomInteger = (min, max) => {
   return Math.floor(result);
 };
 
-const createGeneratorRandomNumber = (min, max)=> {
-  let randomValue;
-  return function () {
-    randomValue = generateRandomInteger(min, max);
-    return randomValue;
-  };
-};
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const generatePhotoId = createNumberId(25);
 const generateNumberForUrl = createNumberId(25);
-const generateCommentId = createNumberId(1000000);
+const generateCommentId = createNumberId();
 
-const createComments = () => {
-  const generateRandomForAvatar = createGeneratorRandomNumber(1, 6);
-  return {
+const createComments = () =>
+  ({
     id: generateCommentId(),
-    avatar: `img/avatar-${ generateRandomForAvatar() }.svg`,
-    message: '1 или 2 случайных предложения из предложенных',
-    username: '',
-  };
-};
+    avatar: `img/avatar-${ getRandomInteger(1, 6) }.svg`,
+    message: getRandomArrayElement(MESSAGE_IN_COMMENTS),
+    username: getRandomArrayElement(USERS_NAMES),
+  });
 
-
-const createDescriptionPhotos = () => {
-  const generateLikes = createGeneratorRandomNumber(15, 200);
-  const generateComments = createGeneratorRandomNumber(0, 30);
-  return {
+const createDescriptionPhotos = () =>
+  ({
     id: generatePhotoId(),
     url: `photos/${ generateNumberForUrl()}.jpg`,
     description: 'Hello! It is my photo!',
-    likes: generateLikes(),
-    comments: Array.from({length: generateComments()}, createComments),
-  };
-};
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({length: getRandomInteger(0, 30)}, createComments),
+  });
 
 const descriptionPhotosList = Array.from({length: 25},createDescriptionPhotos);
 
-console.log(descriptionPhotosList);
+//console.table(descriptionPhotosList);
 
