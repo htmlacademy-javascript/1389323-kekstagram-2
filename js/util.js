869bddab7f187d48e26cc068218fe57ba63e3504
Natalia Popova <ntl230189@gmail.com>
2024-10-31 16:26:07 +0500

    add module5-task1

---
+++ js/util.js
@@ -0,0 +1,21 @@
+const createNumberId = () => {
+  let indexId = 0;
+
+  return function () {
+    indexId += 1;
+    return indexId;
+  };
+};
+
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+
+export {createNumberId, getRandomInteger, getRandomArrayElement};
