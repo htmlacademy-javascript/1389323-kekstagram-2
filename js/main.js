d85a8a8037a01c3c8c05089f4d99efec5603d325
Natalia Popova <ntl230189@gmail.com>
2024-11-19 10:14:15 +0500

    const photosList on main

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import {render as renderPictures, photosList} from './render.js';
+import {render as renderPictures} from './render.js';
 import {descriptionPhotosList} from './data.js';
 import {renderBigPicture} from './render-full-photo.js';
 
-renderPictures(descriptionPhotosList);
+const photosList = renderPictures(descriptionPhotosList);
 const pictureContainer = document.querySelector('.pictures');
 
 renderBigPicture(pictureContainer, photosList);
